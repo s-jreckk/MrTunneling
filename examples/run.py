9b6ee13081d721f696304f5fccd419b6e3242e07
Stephen Goodlett <stephen.goodlett25@gmail.com>
2026-09-21 18:05:46 +0200

    Making run.py make more sense with comments

--- examples/run.py
+++ examples/run.py
@@ -3,9 +3,45 @@ import numpy as np
 from pfxns import PsiRunner
 import mrtunneling
 
+"""
+    Example instanton computation for the CH4 + H ---> CH3 + H2 (doublet electronic state) reaction.
+    Precomputed are the reactant and transition state (TS) optimized geometries.
+    The instanton is initialized from the TS, and then optimized.
+    In the example, the optimization completed on step_39.
+    Exact Hessians are computed, and the rates determined.
+
+"""
+
+# Define task driver
 pRun = PsiRunner()
 
-# CH4 + H TS HF/STO-3G
+# Reactant optimized geometry, get energy, gradient, and Hessian for rate computation
+# CH4 + H Reactant HF/STO-3G
+r_molstr = """
+0 2
+
+C     0.185600783674     -0.015759262400     -0.204237642191
+H     0.723949795324     -0.952705000257     -0.131943770269
+H     0.883125099967      0.808645466307     -0.122182050146
+H    -0.541471531280      0.045513716589      0.596089147682
+H    -0.323201879001      0.035507345366     -1.158913019022
+H    -2.952318219425      0.250681309539      3.248772307704
+
+"""
+
+r_qcmol = qcel.models.Molecule.from_data(r_molstr)
+r_E = pRun.energy(r_qcmol)
+r_G = pRun.gradient(r_qcmol)
+r_H = pRun.hessian(r_qcmol)
+# Manually set the necessary values of the bead, I probably need a better way to do this
+r_bead = mrtunneling.bead.Bead(r_qcmol)
+r_bead._V = r_E
+r_bead.grad = r_G
+r_bead.has_hess = True
+r_bead.exact_hess = True
+r_bead._hess = r_H
+
+# Transition state optimized geometry, get energy, gradient, and Hessian for rate computation and instanton initialization
 molstr = """
 0 2
 
@@ -21,17 +57,18 @@ qcmol = qcel.models.Molecule.from_data(molstr)
 E = pRun.energy(qcmol)
 G = pRun.gradient(qcmol)
 H = pRun.hessian(qcmol)
+# Manually set the necessary values of the bead, I probably need a better way to do this
+ts_bead = mrtunneling.bead.Bead(qcmol)
+ts_bead._V = E
+ts_bead.grad = G
+ts_bead.has_hess = True
+ts_bead.exact_hess = True
+ts_bead._hess = H
 
-#freqs, nmodes = mrtunneling.proc_Hess(qcmol, H)
+# Get frequencies of Hessian (in Hartree)
+freqs, nmodes = mrtunneling.proc_Hess(qcmol, H)
 
-#I.write_to_mXYZ("all.xyz")
-#I.evaluate_all_beads()
-#g = I.gradient()
-#print(g)
-#for bead in I.beads:
-#    bead.energy = pRun.energy
-#    print(bead.energy)
-
+# Define plan for optimizing instanton
 opt_plan = {
         "step_limit": 0.05,
         "max_iter": 50,
@@ -43,21 +80,24 @@ opt_plan = {
         "opt_method": "evf"
         }
 
-#I = mrtunneling.Instanton.initiate_from_TS(400.0, qcmol, H, 16, pRun, delta=0.1)
-#print(I.crossover_T())
-I = mrtunneling.Instanton.restart("step_39", pRun, temp=400.0)
-#I.optimize(opt_plan)
-#print(I.N, len(I.beads))
-#I.double_beads()
-#print(I.N, len(I.beads))
-#I.optimize(opt_plan)
+# Initiate instanton from transition state geometry
+I = mrtunneling.Instanton.initiate_from_TS(400.0, qcmol, H, 16, pRun, delta=0.1)
+# Or restart from directory "step_39"
+#I = mrtunneling.Instanton.restart("step_39", pRun, temp=400.0)
+I.optimize(opt_plan)
+
+# For final iteration, evaluate exact Hessians
 I.evaluate_all_beads(der_lvl=2)
+# Save for future use
 I.save_state("step_39_exact_hess")
-#np.set_printoptions(precision=2, threshold=1000000, linewidth=11900, suppress=True)
-print(np.linalg.eigh(I.full_hessian())[0])
-#MW_full = np.diag(np.concatenate([np.diag(I.Minv()), np.diag(I.Minv())]))
-#MHess = MW_full @ I.full_hessian() @ MW_full
-#print(np.sqrt(np.linalg.eigh(MHess)[0]) / mrtunneling.constants.freq_to_hartree)
+# Or read in if you already have it
+#I = mrtunneling.Instanton.read_state("step_39_exact_hess")
 
+# Set up "fake" ring polymers for rate (r) and tunneling correction (k) computations
+rRP = mrtunneling.fake_ring_polymer.FakeRingPolymer(I, r_bead)
+tsRP = mrtunneling.fake_ring_polymer.FakeRingPolymer(I, ts_bead)
 
+r = mrtunneling.partition_functions.rate(I, rRP)
+k = mrtunneling.partition_functions.kappa(I, tsRP)
+print(r, k)
 
